0a812df07449906c819b7d03d39b0dcfa5db801e
Brad Alexander <brad.alexander913@gmail.com>
2019-08-25 21:05:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, holiday|
-  puts "#{season}:"
+  puts "#{season.capitalize}:"
   holiday.each do |holiday_name, supplies| 
     puts "  #{holiday_name}: #{supplies}"
   end 
