ad8ba45e6bde5c72bd766229d09a62bbabe3ea98
Brad Alexander <brad.alexander913@gmail.com>
2019-08-25 21:10:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ holiday_hash.each do |season, holiday|
     supplies.each_with_index do |supply, index| 
       if ((index + 1) == supply.length)
       puts " #{supply}"
-    else 
+    else " #{supply},"
       print 
     end
   end 
